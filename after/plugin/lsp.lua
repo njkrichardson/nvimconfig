a5bcc85b150d9dad8121524d1420529f26d6e4ee
Nick Richardson <nrichardson@g.hmc.edu>
2023-07-21 11:29:26 -0700

    adds enter as lsp autocomplete selection

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -11,17 +11,15 @@ lsp.ensure_installed({
 })
 
 local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
-local cmp_mappings = lsp.defaults.cmp_mappings({
-	['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-	['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-	['<C-y>'] = cmp.mapping.confirm({ select = true }),
-	['<C-Space>'] = cmp.mapping.complete(),
+cmp.setup({
+    mapping = {
+        ['<CR>'] = cmp.mapping.confirm({ select = false }), 
+    }
 })
 
- lsp.set_preferences({
+lsp.set_preferences({
  	sign_icons = { }
- })
+})
 
 lsp.on_attach(function(client, bufnr)
 	local opts = {buffer = bufnr, remap = false}
